bf7e81cc50d50a588627781fa7e786022e033d19
NHHoangTuan <hoangtuan.study@gmail.com>
2024-10-21 20:31:53 +0700

    define colors

---
+++ jarvis_ktk/lib/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBlue = Color(0xFFBBDEFB);  // Xanh dương nhạt
+  static const Color blue = Color(0xFF2196F3);       // Xanh dương chuẩn
+  static const Color darkBlue = Color(0xFF1976D2);   // Xanh dương đậm
+  static const Color navyBlue = Color(0xFF0D47A1);   // Xanh dương cực đậm
+  static const Color lightBlueTransparent = Color(0x9982B1FF); // Xanh dương nhạt trong suốt
+  static const Color skyBlue = Color(0xFF87CEEB);    // Xanh da trời
+  static const Color deepSkyBlue = Color(0xFF00BFFF); // Xanh da trời đậm
+  static const Color dodgerBlue = Color(0xFF1E90FF);  // Xanh dương Dodger
+  static const Color steelBlue = Color(0xFF4682B4);   // Xanh dương thép
+  static const Color royalBlue = Color(0xFF4169E1);   // Xanh dương hoàng gia
+  static const Color mediumBlue = Color(0xFF0000CD);  // Xanh dương trung bình
+  static const Color slateBlue = Color(0xFF6A5ACD);   // Xanh dương đá phiến
+  static const Color cornflowerBlue = Color(0xFF6495ED); // Xanh dương hoa thanh cúc
+  static const Color indigoBlue = Color(0xFF4B0082);   // Xanh dương chàm
+  static const Color babyBlue = Color(0xFF89CFF0);    // Xanh dương baby
+}
